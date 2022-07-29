eceedfeb77302c2bd70df6ff0a47003feb352353
Vanessa <vanymartins2@gmail.com>
2022-07-29 11:33:50 -0300

    fixed connection string

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,6 @@
 import { Pool } from 'pg'
 
-const connectionString =
-  'postgres://wtdepltr:0qNBTvEN8n1fnCUEfO3rHZrxkkT3frWv@kesavan.db.elephantsql.com/wtdepltr'
+const connectionString = '{connectionString}'
 
 const db = new Pool({ connectionString })
 
